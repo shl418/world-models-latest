38f01bb558f7661db9b859860f52546b80346655
Pantelis Monogioudis <monogiou@njit.edu>
2021-04-28 07:35:50 -0400

    updated with graphviz and python version check_vae

--- launch-docker-gpu.sh
+++ launch-docker-gpu.sh
@@ -1,2 +1,10 @@
 # USAGE - ./launch-docker-gpu.sh {abs-path-to-WorldModels-code}
-docker run --rm --gpus all  --network=host -it -v $1:/Worldmodels worldmodels-image
+docker run --rm -it \
+    --gpus all \
+    --network=host \
+    -v $1:/Worldmodels \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \    
+    worldmodels-image:latest
+
